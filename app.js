eafc82db6a2ab714b5086353c2d6f5117735ee39
unknown <kqyz12@gmail.com>
2024-11-05 16:43:14 +0300

    setup files

--- app.js
+++ app.js
@@ -83,3 +83,18 @@ const basicCalculator = (num1, num2, operation) => {
   }
 }
 console.log('Exercise 9 Result:', basicCalculator(10, 5, 'multiply'))
+
+const calculateGrade = (num) => {
+  if (num >= 90 && num <= 100) {
+    return 'A'
+  } else if (num >= 80 && num <= 89) {
+    return 'B'
+  } else if (num >= 70 && num <= 79) {
+    return 'C'
+  } else if (num >= 60 && num <= 69) {
+    return 'D'
+  } else if (num >= 0 && num <= 59) {
+    return 'F'
+  }
+}
+console.log('Exercise 10 Result:', calculateGrade(1))
