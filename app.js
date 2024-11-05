0716d70511dc609e625616fc615e8d5808d7adf0
unknown <kqyz12@gmail.com>
2024-11-05 12:17:09 +0300

    setup files

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Its,s working')
+const maxOfTwoNumbers = (x, y) => {
+  if (x >= y) {
+    return x
+  } else {
+    return y
+  }
+}
+
+console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9))
+
+const isAdult = (age) => {
+  if ((age) => 18) {
+    return 'adult'
+  } else {
+    return 'Minor'
+  }
+}
+
+console.log('Exercise 2 Result:', isAdult(21))
